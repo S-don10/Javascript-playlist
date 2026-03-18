27485f468d8a605134f2d1c1e98262c2259ac32c
S-don10 <subhendu.bairagya.mec23@itbhu.ac.in>
2026-03-18 14:15:17 +0000

    done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId=12435
+let accountEmail="subhendu@google.com";
+let accountPassword="12345";
+let accountCity="Jaipur";
+let accountState;
+// prefer do not use var 
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
