7bd84af2cb4bbdc5cd8bfe37dad5053591b7207f
S-don10 <subhendu.bairagya.mec23@itbhu.ac.in>
2026-03-18 14:46:36 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+
+"use strict"//use all js code as newer version
+//alert(3+3) //we are useing node.js not browserlet age=21;
+
+
+
+let name="subhendu";
+let age=20;
+let isLoggedIn= false //booliean
+// Some of datatypes---------->
+//number => 2 to power 53
+//string => ""
+//boolean => true/false
+//null => standalone value or empty value
+//undefined =>
+//symbol =>unique
+//object
+console.log(typeof undefined); // undefined
+console.log(typeof null);// object
