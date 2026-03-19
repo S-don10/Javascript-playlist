27d59deb2b18383abfd659d07c732f4571bc267d
S-don10 <subhendu.bairagya.mec23@itbhu.ac.in>
2026-03-19 10:24:18 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,25 @@
+// let score="1900";
+// let stringNumber=Number(score)
+// console.log(stringNumber);
+
+// console.log(typeof stringNumber);
+// "33"=> 33
+// "33abc"=>NaN
+// true=>1 and false convert into 0
+
+
+let loggedIn="";
+let changeBollean = Boolean(loggedIn);
+
+// console.log( changeBollean);
+// 1--> true and 0 --> false
+// " "=> false
+// "subhendu"=> true;
+
+let someNUmber=undefined;
+let convertString = String(someNUmber);
+console.log(convertString);
+console.log(typeof convertString);
+
+
+
